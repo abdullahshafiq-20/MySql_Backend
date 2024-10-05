52b73105c3bcdd539e459daa8460bee3cb004488
Abdullah Shafiq <as053266@gmail.com>
2024-10-05 23:31:14 +0500

    updated

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5173", 
+    origin: "*", 
 
     methods: ["GET", "POST", "PUT", "DELETE"]
   }
